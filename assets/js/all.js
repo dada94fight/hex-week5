8955ffd9468365c9e59d87fab922ddf7d664fb54
snowcherry <snowcherry919@gmail.com>
2020-06-17 20:15:51 +0800

    Update 2020-06-17T12:15:45.995Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,31 +1,24 @@
-$(document).ready(() => {
+"use strict";
+
+$(document).ready(function () {
   // console.log('Hello Bootstrap4');
-
-  $('#reply_btn').on('click', function(){
-      $('#reply_container').css("display","block");
-    });
-
-
-  $('#cancel_btn').on('click', function(){
-    $('#reply_container').css("display","none");
+  $('#reply_btn').on('click', function () {
+    $('#reply_container').css("display", "block");
   });
-
-// admin detail 視窗區塊
-
+  $('#cancel_btn').on('click', function () {
+    $('#reply_container').css("display", "none");
+  }); // admin detail 視窗區塊
   //寫法一 (可正常展開和收回，但無法改變icon)
   // $('#expand_more').on('click', function(){
   //   $('#expand_moreIcon').toggleClass(function(){
   //     $('#expand_moreIcon').text('expand_less');
   //   });
   // })
-  
   //寫法二 (可以正常展開且改變一次icon，但需要點按鈕的邊緣才能收回，且icon不會變回來)
   // $('#expand_more').on('click', function(){
   //   $("#expand_moreIcon").toggleClass(function(){
   //     $('#expand_moreIcon').html('<span class="material-icons text-primary" id="expand_moreIcon">expand_less</span>');
   //   });
   // });
-
-
 });
-
+//# sourceMappingURL=all.js.map
